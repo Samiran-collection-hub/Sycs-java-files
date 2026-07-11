5bfccc11023cb6dd691dfb6dc223e1ba2e081606
Samiran-collection-hub <samirdhanke53@gmail.com>
2026-07-11 19:36:00 +0530

    Create DateEx.java
    practical 3 part c

---
+++ DateEx.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class DateEx {
+    public static void main(String[] args) {
+      Date now = new Date();
+      System.out.println("Current date is :"+now);
+    }
+}
