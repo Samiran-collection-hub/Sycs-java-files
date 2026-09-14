579ac649ac2c9de7386cb017bc117543c4c41c1b
Samiran-collection-hub <samirdhanke53@gmail.com>
2026-09-14 11:41:46 +0530

    Create MatrixVectorOp.java

---
+++ MatrixVectorOp.java
@@ -0,0 +1,22 @@
+import jdk.incubator.vector.FloatVector;
+import jdk.incubator.vector.VectorSpecies;
+
+public class MatrixVectorOp {
+    static final VectorSpecies<Float> SPECIES = FloatVector.SPECIES_PREFERRED;
+
+    public static void main(String[] args) {
+        float[] a = {1.0f, 2.0f, 3.0f, 4.0f};
+        float[] b = {5.0f, 6.0f, 7.0f, 8.0f};
+        float[] result = new float[4];
+
+        // Perform SIMD vector addition across arrays (matrix rows)
+        for (int i = 0; i < a.length; i += SPECIES.length()) {
+            var va = FloatVector.fromArray(SPECIES, a, i);
+            var vb = FloatVector.fromArray(SPECIES, b, i);
+            var vc = va.add(vb); // Inbuilt vector operation
+            vc.intoArray(result, i);
+        }
+
+        System.out.println("Result: " + java.util.Arrays.toString(result));
+    }
+}
