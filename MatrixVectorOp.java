14f5e2de7a4e9fd6705a69db5a865340ade9f763
Samiran-collection-hub <samirdhanke53@gmail.com>
2026-09-16 12:17:24 +0530

    Update MatrixVectorOp.java

--- MatrixVectorOp.java
+++ MatrixVectorOp.java
@@ -4,8 +4,7 @@ import jdk.incubator.vector.VectorSpecies;
 public class MatrixVectorOp {
     static final VectorSpecies<Float> SPECIES = FloatVector.SPECIES_PREFERRED;
     public static void main(String[] args) {
-        float[] a = {1.0f, 2.0f, 3.0f, 4.0f};
-        float[] b = {5.0f, 6.0f, 7.0f, 8.0f};
+        float[] a = {1.0f, 2.0f, 3.0f, 4.0f},b = {5.0f, 6.0f, 7.0f, 8.0f};
         float[] result = new float[4];
         for (int i = 0; i < a.length; i += SPECIES.length()) {
             var va = FloatVector.fromArray(SPECIES, a, i);
