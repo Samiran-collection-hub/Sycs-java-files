c17915b9941f8f59ad43c8cda8648b79cc27c214
Samiran-collection-hub <samirdhanke53@gmail.com>
2026-09-20 16:56:14 +0530

    Update MatrixVectorOp.java

--- MatrixVectorOp.java
+++ MatrixVectorOp.java
@@ -5,8 +5,7 @@ public class MatrixVectorOp {
     public static void main(String[] args) {
         float[] a = {1.0f, 2.0f, 3.0f, 4.0f},b = {5.0f, 6.0f, 7.0f, 8.0f},result = new float[4];
         for (int i = 0; i < a.length; i += SPECIES.length()) {
-            var va = FloatVector.fromArray(SPECIES, a, i);
-            var vb = FloatVector.fromArray(SPECIES, b, i);
+            var va = FloatVector.fromArray(SPECIES, a, i),vb = FloatVector.fromArray(SPECIES, b, i);
             var vc = va.add(vb); 
             vc.intoArray(result, i);
         }
